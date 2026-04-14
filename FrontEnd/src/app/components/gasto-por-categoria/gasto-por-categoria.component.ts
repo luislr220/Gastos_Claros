1e586c2b5ccf5767594fad5b2b4ff70e17476726
luislr220 <largoramirezluisarmando@gmail.com>
2026-04-13 18:38:50 -0600

    Agregando la funcionalidad de charPie

--- FrontEnd/src/app/components/gasto-por-categoria/gasto-por-categoria.component.ts
+++ FrontEnd/src/app/components/gasto-por-categoria/gasto-por-categoria.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, viewChild } from '@angular/core';
+import { Chart, PieController, ArcElement, Tooltip, Legend } from 'chart.js';
 import { GastosService } from '../../../services/gastos.service';
 import { GastoPorCategoria } from '../../../models/Gasto';
+
+Chart.register(PieController, ArcElement, Tooltip, Legend);
 
 @Component({
   selector: 'app-gasto-por-categoria',
@@ -9,6 +12,12 @@ import { GastoPorCategoria } from '../../../models/Gasto';
   styleUrl: './gasto-por-categoria.component.css',
 })
 export class GastoPorCategoriaComponent implements OnInit {
+  canvasRef = viewChild<ElementRef>('pieCanvas');
+
+  chart: any;
+  nombresGastos: any = [];
+  precioGastos: any = [];
+
   constructor(private readonly gastoService: GastosService) {}
 
   gastoCategoria: GastoPorCategoria = {};
@@ -24,6 +33,7 @@ export class GastoPorCategoriaComponent implements OnInit {
   gastoPorCategoria() {
     this.gastoService.getGastoPorCategoria().subscribe({
       next: (response: any) => {
+        console.log(response);
         /* Aqui solo devolvi null ya que como maneja el mismo mensaje que listado de gastos en la misma vista 
           Seria redundante tener el mismo mensaje.
         */
@@ -31,6 +41,10 @@ export class GastoPorCategoriaComponent implements OnInit {
           return;
         }
         this.gastoCategoria = response.data;
+        this.nombresGastos = Object.keys(this.gastoCategoria);
+        this.precioGastos = Object.values(this.gastoCategoria);
+
+        this.crearChart();
       },
       error: (e: any) => {
         console.error('Error al obtener el gasto por categoria: ', e);
@@ -45,5 +59,27 @@ export class GastoPorCategoriaComponent implements OnInit {
   capitaizar(texto: string) {
     if (!texto) return '';
     return texto.charAt(0).toUpperCase() + texto.slice(1).toLowerCase();
+  }
+
+  crearChart() {
+    const elemento = this.canvasRef();
+
+    if (elemento) {
+      this.chart = new Chart(elemento.nativeElement, {
+        type: 'pie',
+        data: {
+          labels: this.nombresGastos.map((nombre: string) =>
+            this.capitaizar(nombre),
+          ),
+          datasets: [
+            {
+              label: 'Votos',
+              data: this.precioGastos,
+              backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+            },
+          ],
+        },
+      });
+    }
   }
 }
